b0d3dea291770f52cd00be847ff349ef6ad2a2b2
汪腾飞 <tony_xf@foxmail.com>
2021-10-27 22:58:43 +0800

    添加移动事件

--- src/lib/ShootGame.js
+++ src/lib/ShootGame.js
@@ -2,9 +2,22 @@ import threeLib from './threeLib'
 
 export const shootGame = function(container){
     function createPlane(){
-        const geometry = new THREE.PlaneBufferGeometry(120, 100, 32, 32)
+        const geometry = new THREE.PlaneBufferGeometry(1000, 2000, 32, 32)
         const material = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide})
         return new THREE.Mesh(geometry, material)
+    }
+    const keyCode = {}
+    function addEvent(){
+        document.addEventListener('keydown', (event)=>{
+            if(!keyCode[event.keyCode]){
+                keyCode[event.keyCode] = true
+            }
+        }, false)
+        document.addEventListener('keyup', (event)=>{
+            if(keyCode[event.keyCode]){
+                keyCode[event.keyCode] = false
+            }
+        }, false)
     }
     class ShootGame extends  threeLib{
         constructor(container) {
@@ -12,17 +25,34 @@ export const shootGame = function(container){
             this.createOrbitControls()
             this.initModel()
             this.setCamera()
+            addEvent()
         }
         setCamera(){
-            this.camera.position.set(0, 50, 50)
+            this.camera.position.set(0, 100, 0)
         }
-        initEvent(){
-
+        rotateCamera(){
+            if(keyCode[65]){
+                this.camera.rotation.y += Math.PI/72
+            }
+            if(keyCode[68]){
+                this.camera.rotation.y -= Math.PI/72
+            }
         }
         initModel(){
             const plane = createPlane()
             plane.rotation.x = -Math.PI/2
             this.scene.add(plane)
+        }
+        render(){
+            const animation = ()=>{
+                this.rotateCamera()
+                this.renderer.render(this.scene, this.camera)
+                // if(this.orbitControls){
+                //     this.orbitControls.update()
+                // }
+                requestAnimationFrame(animation)
+            }
+            animation()
         }
     }
     return new ShootGame(container)
