dd06c8ca8ba720288796b5902e94da04d02023cb
汪腾飞 <wc_wtf@laozicloud.com>
2021-11-02 17:59:09 +0800

    添加汽车模型

--- src/lib/ShootGame.js
+++ src/lib/ShootGame.js
@@ -1,8 +1,8 @@
 import threeLib from './threeLib'
-
+import {FBXLoader} from "three/examples/jsm/loaders/FBXLoader";
 export const shootGame = function(container){
     function createPlane(){
-        const geometry = new THREE.PlaneBufferGeometry(1000, 2000, 32, 32)
+        const geometry = new THREE.PlaneBufferGeometry(2000, 2000, 32, 32)
         const material = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide})
         return new THREE.Mesh(geometry, material)
     }
@@ -19,6 +19,30 @@ export const shootGame = function(container){
             }
         }, false)
     }
+    function createPath(){
+        const curve = new THREE.CatmullRomCurve3([
+            new THREE.Vector3( 0, 0, 50 ),
+            new THREE.Vector3( -100, 0, 0 ),
+            new THREE.Vector3( 0, 0, -50 ),
+            new THREE.Vector3( 100, 0, 0 ),
+        ])
+        const points = curve.getPoints(500);
+        const geometry = new THREE.BufferGeometry().setFromPoints(points)
+        const material = new THREE.LineBasicMaterial({color: 0xff0000})
+        return {
+            points,
+            mesh:new THREE.Line(geometry, material)
+        }
+    }
+    function createCar(){
+        const loader = new FBXLoader()
+        const base = process.env.BASE_URL
+        return new Promise((resolve, reject)=>{
+            loader.load(`${base}models/gltf/car.FBX`, function(object){
+                resolve(object.children[0])
+            })
+        })
+    }
     class ShootGame extends  threeLib{
         constructor(container) {
             super(container);
@@ -26,9 +50,12 @@ export const shootGame = function(container){
             this.initModel()
             this.setCamera()
             addEvent()
+            this.createLight()
         }
         setCamera(){
-            this.camera.position.set(0, 100, 0)
+            this.camera.far = 5000;
+            this.camera.position.set(0, 100, 300)
+            this.camera.updateProjectionMatrix()
         }
         rotateCamera(){
             if(keyCode[65]){
@@ -39,12 +66,44 @@ export const shootGame = function(container){
             }
         }
         initModel(){
+            //this.scene.rotation.x = -Math.PI/2
             const plane = createPlane()
             plane.rotation.x = -Math.PI/2
             this.scene.add(plane)
+            const path = createPath()
+            this.points = path.points
+            this.scene.add(path.mesh)
+            createCar().then((mesh)=>{
+                console.log(mesh)
+                mesh.name = 'car'
+                mesh.scale.set(0.08,0.08,0.08)
+                mesh.position.set(this.points[0].x, this.points[0].y, this.points[0].z);
+                mesh.lookAt(this.points[1].x,this.points[1].z,this.points[1].y)
+                mesh.rotation.x = -Math.PI/2
+                mesh.rotation.z = -Math.PI/4
+                this.car = mesh
+
+                const axesHelper = new THREE.AxesHelper(500);
+                mesh.add(axesHelper)
+                this.scene.add(mesh)
+            })
         }
         render(){
+            const size = this.points.length
+            let i = 0
             const animation = ()=>{
+                if(i < size-1){
+                    i++;
+                    if(this.car){
+                        this.car.position.set(this.points[i].x, this.points[i].y, this.points[i].z)
+                        if(this.points[i+1]){
+                            this.car.lookAt(this.points[i+1].x,this.points[i+1].z,this.points[i+1].y);
+                        }else{
+                            this.car.lookAt(this.points[i].x,this.points[i].z,this.points[i].y);
+                        }
+                        this.car.rotation.x = -Math.PI/2
+                    }
+                }
                 this.rotateCamera()
                 this.renderer.render(this.scene, this.camera)
                 // if(this.orbitControls){
